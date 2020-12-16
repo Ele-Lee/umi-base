01e2151d8084bbc188d64da69aa5490d0682c02a
ELe_Lee <liyilei@guorou.tv>
2020-12-16 11:46:32 +0800

    change git

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/elelee/Desktop/projects/portal_base/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','request','qiankun',],
-});
-
-export { plugin };
--- .umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/elelee/Desktop/projects/portal_base/node_modules/react-helmet';
